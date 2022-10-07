60d8ca1e9dc7e91d3a1f6f80f15cfbcd18ab649b
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-10-07 19:19:00 +0300

    commit

---
+++ cardio/src/utils/fetchData.js
@@ -0,0 +1,37 @@
+export const exercisesOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+  
+   export const youtubeUrloptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
+  
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://fitness-calculator.p.rapidapi.com/activities',
+  //   params: {intensitylevel: '10'},
+  //   headers: {
+  //     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  //     'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+  //   }
+  // };
+  export const bmioptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key':process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+    }
+  };
+  export const fetchData = async (url, option) => {
+      const response = await fetch(url, option);
+      const data = await response.json();
+      return data;
+  }
