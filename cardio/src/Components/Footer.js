e305fee852d7b0c78b14cf4d19e182d520be29c9
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-10-07 19:00:57 +0300

    change

---
+++ cardio/src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box, Typography, Stack } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
+const Footer = () => {
+  return (
+    <Box mt='80px' bgColor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt="logo" width='200px' height='40px' />
+        <Typography variant='h3' pb='40px' mt='40px'>
+        
+        </Typography>
+      </Stack>
+    </Box>
+  )
+}
+
+export default Footer
