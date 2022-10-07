f5540c253c7de2624ce429d09eb313887a669621
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-10-07 20:44:12 +0300

    commit

--- cardio/src/App.js
+++ cardio/src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Box } from '@mui/material'
 import  './App.css'
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 import Home from './Pages/Home';
 import ExerciseDetails from './Pages/ExerciseDetails';
